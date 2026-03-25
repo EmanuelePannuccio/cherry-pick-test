0c4c6dd97205ec52ef3405a8c8b63fee0b72238d
Your Name <you@example.com>
2026-03-25 14:52:09 +0100

    feat(A): aggiunta moltiplicazione

--- test.py
+++ test.py
@@ -6,7 +6,11 @@ def add(x, y):
 def subtract(x, y):
     return x - y
 
+def multiply(x, y):
+    return x * y
+
 if __name__ == "__main__":
     print("Calcolatrice v1.0 - Base")
     print(f"5 + 3 = {add(5, 3)}")
     print(f"5 - 3 = {subtract(5, 3)}")
+    print(multiply(4,5))
